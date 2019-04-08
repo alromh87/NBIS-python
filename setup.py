1e983c332ebebf8854fdaf30b970f2296c0539b3
Guillermo Bonvehí <gbonvehi@gmail.com>
2019-04-08 15:06:50 -0300

    build pyNBIS package with wqs, lfs, nfiq, bozorth

--- setup.py
+++ setup.py
@@ -4,28 +4,28 @@
 setup.py file for NBIS SWIG wrapper
 """
 
-from setuptools import setup, Extension
+from setuptools import setup, find_packages, Extension
 
 NBIS_DIR='./install_dir/'
 NBIS_INCLUDE_DIR=NBIS_DIR+'include/'
 NBIS_LIB_DIR=NBIS_DIR+'lib/'
 
-wsq_module = Extension('_wsq', ['src/wsq.i'], swig_opts=['-I'+NBIS_INCLUDE_DIR],
+wsq_module = Extension('_wsq', ['pyNBIS/wsq.i'], swig_opts=['-I'+NBIS_INCLUDE_DIR],
                       include_dirs=[NBIS_INCLUDE_DIR],
                       library_dirs=[NBIS_LIB_DIR],
                       libraries=['wsq', 'ioutil', 'fet', 'jpegl', 'util'],
                       )
-nfiq_module = Extension('_nfiq', ['src/nfiq.i'], swig_opts=['-I'+NBIS_INCLUDE_DIR],
+nfiq_module = Extension('_nfiq', ['pyNBIS/nfiq.i'], swig_opts=['-I'+NBIS_INCLUDE_DIR],
                       include_dirs=[NBIS_INCLUDE_DIR],
                       library_dirs=[NBIS_LIB_DIR],
                       libraries=['nfiq', 'mindtct', 'mlp', 'cblas', 'ioutil', 'util'],
                       )
-lfs_module = Extension('_lfs', ['src/lfs.i'], swig_opts=['-I'+NBIS_INCLUDE_DIR],
+lfs_module = Extension('_lfs', ['pyNBIS/lfs.i'], swig_opts=['-I'+NBIS_INCLUDE_DIR],
                       include_dirs=[NBIS_INCLUDE_DIR],
                       library_dirs=[NBIS_LIB_DIR],
                       libraries=['mindtct', 'mlp', 'cblas', 'ioutil', 'util', 'an2k', 'ioutil', 'image', 'util', 'bozorth3'],
                       )
-bozorth_module = Extension('_bozorth', ['src/bozorth.i'], swig_opts=['-I'+NBIS_INCLUDE_DIR],
+bozorth_module = Extension('_bozorth', ['pyNBIS/bozorth.i'], swig_opts=['-I'+NBIS_INCLUDE_DIR],
                       include_dirs=[NBIS_INCLUDE_DIR],
                       library_dirs=[NBIS_LIB_DIR],
                       libraries=['bozorth3', 'mindtct', 'mlp', 'cblas', 'ioutil', 'util'],
@@ -38,7 +38,8 @@ setup (name = "pyNBIS",
        author_email	= "alromh87@gmail.com",
        description	= """SIWG based Python wrapper for NBIS""",
        packages		= ["pyNBIS"],
-       package_dir = {'pyNBIS': 'src'},
+       # package_dir = {'pyNBIS': 'src'},
+       # packages=find_packages(where="src"),
        ext_modules	= [wsq_module, nfiq_module, lfs_module, bozorth_module],
        py_modules	= ["wsq", "nfiq", "lfs", "bozorth"],
        )
